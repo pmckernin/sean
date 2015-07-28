92fa7d0480474cd569968b9547e1ad9fcf67a558
pmckernin <pmckernin@gmail.com>
2015-07-28 17:13:02 -0500

    Facebook login is working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727192239) do
+ActiveRecord::Schema.define(version: 20150728201051) do
 
   create_table "attendances", force: :cascade do |t|
     t.string   "user_uid"
@@ -73,9 +73,11 @@ ActiveRecord::Schema.define(version: 20150727192239) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["provider"], name: "index_users_on_provider"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
