fc793b4780b8428c1b2b2a970fae58f588747ed3
pmckernin <pmckernin@gmail.com>
2015-07-28 23:36:19 -0500

    User friendship relations finished

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ActiveRecord::Base
+
 def sender
  sender =  User.where(:uid => sender_uid)
  sender.first
@@ -9,11 +10,6 @@ def reciever
   reciever.first
 end
 
- # has sender_id and receiver_id columns to store User IDs
-  #belongs_to :sender, :class_name => "User" , :foreign_key => "sender_uid"
 
-  #belongs_to :receiver, :class_name => "User" , :foreign_key => "receiver_uid"
 
-  # has accepted:boolean column
-  #scope :accepted, -> { where(:accepted => true) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,39 +5,52 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
-
-  #has_many :friend_requests_as_sender, :class_name => "Friendship", :foreign_key => "sender_uid"
-  #has_many :friend_requests_as_sender_accepted, -> { accepted }, :class_name => "Friendship", :foreign_key => "sender_uid"
-  #has_many :sent_friends, :through => :friend_requests_as_sender_accepted, :source => :receiver
-
-  #has_many :friend_requests_as_receiver, :class_name => "Friendship", :foreign_key => "receiver_uid"
-  #has_many :friend_requests_as_receiver_accepted, -> { accepted }, :class_name => "Friendship", :foreign_key => "receiver_uid"
-  #has_many :received_friends, :through => :friend_requests_as_receiver_accepted, :source => :sender
-
-
+#Returns a list of all friendships you initated / sent to another user
 def sent_friends
    Friendship.where(:sender_uid => self.uid, :accepted => true)
-
 end
 
+#Returns a list of all friend confirmed friendships you received
 def received_friends
   Friendship.where(:reciever_uid => self.uid, :accepted => true)
-
 end
 
+#Returns a list of everyone you are confirmed friends with
   def friends
     friend_uids = []
     friend_uids = friend_uids + self.sent_friends.pluck(:reciever_uid)
     friend_uids = friend_uids + self.received_friends.pluck(:sender_uid)
-
     return User.where(:uid => friend_uids)
   end
 
+#Returns pending friendship requests you sent to other users
+def sent_friend_requests
+   Friendship.where(:sender_uid => self.uid, :accepted => false)
+end
 
+#Returns pending friendship requests you are recieveing from other users
+def received_friend_requests
+  Friendship.where(:reciever_uid => self.uid, :accepted => false)
+end
+
+#Returns a list of Users you have recieved friend requests from
+def pending_recieved_friend_requests
+    friend_uids = []
+    friend_uids = friend_uids + self.received_friend_requests.pluck(:sender_uid)
+    return User.where(:uid => friend_uids)
+end
+
+#Returns a list of Users you have SENT friend requests
+def pending_sent_friend_requests
+    friend_uids = []
+    friend_uids = friend_uids + self.sent_friend_requests.pluck(:reciever_uid)
+    return User.where(:uid => friend_uids)
+end
+
+
+#Recieves the following info from facebook when a new user is created
 def self.from_omniauth(auth)
-
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
     user.first_name = auth.info.first_name
