4a1f8b889d0674025e3262730fae4241466bc0d4
pmckernin <pmckernin@gmail.com>
2015-07-28 23:49:57 -0500

    Added some comments to a few model files

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,10 +1,12 @@
 class Friendship < ActiveRecord::Base
 
+#Returns the User who has SENT the friendship request
 def sender
  sender =  User.where(:uid => sender_uid)
  sender.first
 end
 
+#Returns the User who has RECIEVED the friendship request
 def reciever
   reciever = User.where(:uid => reciever_uid)
   reciever.first
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,13 +1,13 @@
 class Invite < ActiveRecord::Base
   belongs_to :event
 
-  #Finds the sender of the event
+  #Finds the sender of the invite
   def sender
    sender = User.where(:uid => sender_uid)
    sender.first
   end
 
-#Finds the reciever of the event
+#Finds the reciever of the invite
 def recipient
   recipient = User.where(:uid => recipient_uid)
   recipient.first
