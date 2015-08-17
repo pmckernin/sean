ef62f78bf46756228b31ff6960bdd97441e3e68c
pmckernin <pmckernin@gmail.com>
2015-08-17 18:57:20 -0500

    working on front end of new event

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -5,7 +5,13 @@ class FriendsController < ApplicationController
   end
 
   def recommended
+  @user = User.all
 
+    if user_signed_in?
+      Koala.config.api_version = 'v2.0'
+      @graph = Koala::Facebook::API.new(session["token"])
+      @friends = @graph.get_connections("me", "friends")
+    end
   end
 
   def pending
